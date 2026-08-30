2e48e4fcf3f75b78520da6333fdad941f46a5fdf
luyanda-03 <luyandaa.mthembu03@gmail.com>
2026-08-30 09:36:36 +0200

    add track points function. fix: bug fix

--- main.py
+++ main.py
@@ -25,9 +25,15 @@ pos = tuple()
 black = (0,0,0)
 white = (255, 255, 255)
 
+# variables to keep track of points
+player1_score = player2_score = 0
+checked_indexes = []
 
 # Variable to control loop
 running = True 
+
+# change captipn
+pygame.display.set_caption("Seega")
 
 # all functions are placed under this comment
 
@@ -42,34 +48,62 @@ def board():
 def draw_markers():
     for i in range(ROWS):
         for j in range(COLS):
-            centre_x = (((i+1) +i) / 2) * 100
-            centre_y = (((j+1) +j) / 2) * 100
+            centre_x = (((j+1) +j) / 2) * 100
+            centre_y = (((i+1) +i) / 2) * 100
             if markers[i][j] == 1:
                 pygame.draw.circle(screen, black, (centre_x, centre_y),30)
             elif markers[i][j] == 2:
                  pygame.draw.circle(screen, white, (centre_x, centre_y),30)
-            
+  
+# check for points
+def track_points():
+
+    global player2_score, player1_score
+
+    # Horizontal check
+    for i in range(len(markers)):
+        groups = [(markers[i][j], markers[i][j+1], markers[i][j+2]) for j in range(len(markers[i]) -2)]
+        
+        if (1,2,1) in groups:
+            player1_score += 1
+        elif (2,1,2) in groups:
+            player2_score += 1
+    
+        
+    
+    # Vertical check
+    for j in range(COLS):
+        groups = [(markers[i][j], markers[i+1][j], markers[i+2][j]) for i in range(ROWS -2)]
+
+        if (1,2,1) in groups:
+            player1_score += 1
+        elif (2,1,2) in groups:
+            player2_score += 1
 
 while running:
 
     # screen.fill(board_colour)
     board()
     draw_markers()
+    # track_points()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN and not clicked:
-             clicked = True
+            clicked = True
         if event.type == pygame.MOUSEBUTTONUP and clicked:
-             clicked = False
-             pos = pygame.mouse.get_pos() #get x,y coordinate of the mouse position
-             x,y = pos
+            clicked = False
+            pos = pygame.mouse.get_pos() # get x,y coordinate of the mouse position
+            x,y = pos
 
-             if markers[x//square_wid][y // square_height] == 0:
-                  markers[x//square_wid][y // square_height] = player
-                  player = (player % 2) + 1
-                  
+            if markers[y//square_wid][x // square_height] == 0:
+                markers[y//square_wid][x // square_height] = player
+                player = (player % 2) + 1
+            track_points() # Only adjust the scores after the player has made a move to prevent the score from being update for every loop. 
                 
     pygame.display.update()
 
 print(markers)
+
+print(player1_score)
+print(player2_score)
